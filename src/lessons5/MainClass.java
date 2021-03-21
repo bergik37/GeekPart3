7f8342db2ea8422bfe6c4e0accb035f5b6974de3
Aleksndr <bergik-92@mail.ru>
2021-03-21 20:28:56 +0300

    d/z 5

--- src/lessons5/MainClass.java
+++ src/lessons5/MainClass.java
@@ -5,7 +5,7 @@ import java.util.concurrent.CyclicBarrier;
 
 public class MainClass {
     public static final int CARS_COUNT = 4;
-    public static final int HALF_CARS_COUNT = CARS_COUNT/2;
+    public static final int HALF_CARS_COUNT = CARS_COUNT / 2;
 
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
@@ -14,7 +14,7 @@ public class MainClass {
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10),cb, cdl);
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cb, cdl);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
